8a5cd5169ed2c462fb65398ab362f78c2278efb6
Niravvasani12 <Niravvasani12@users.noreply.github.com>
2025-12-22 11:11:58 +0530

    new Comment

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.post("/signup", (req, res) => {
   const { name, email, password } = req.body;
   // Later you will save to DB
+  //** Its required To All the database are store in our Mongo Db atlas */
   res.json({ message: "User registered successfully", user: { name, email } });
 });
 
